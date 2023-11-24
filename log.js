79b3e16ed56136d7626204edcd079166b08c0b53
hilmy2583 <muhammadhilmy2583@gmail.com>
2023-11-24 09:25:36 +0700

    Praktik 08 dan starting node js

---
+++ log.js
@@ -0,0 +1,11 @@
+console.log("Hello World from Node.js");
+
+const nama = "Muhammad Hilmy";
+
+console.log("Nama saya " + nama);
+
+const jurusan = "Teknik Infromatika";
+
+const angkatan = 2022;
+
+console.log("Saya Jurusan " + jurusan + "\nAngkatan " + angkatan);
